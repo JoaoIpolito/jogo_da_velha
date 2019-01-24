da346a291a47633c110007d59c9dabd74739ba58
marcelomeneses <marcelomeneses@pop-os.localdomain>
2019-01-24 08:19:06 -0300

    removendo importes

--- src/jogodavelha/JJogoDaVelha.java
+++ src/jogodavelha/JJogoDaVelha.java
@@ -1,16 +1,12 @@
 package jogodavelha;
 
-import java.awt.Color;
 import java.awt.Container;
-import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
 
 /**
  *
